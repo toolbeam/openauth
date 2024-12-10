88bd390e1399f38dcb14e13142271f0c3d52ace1
Rajesh Gollapudi <rajesh.gollapudi@3ds.com>
2024-12-10 13:33:24 -0500

    feature: add sqlite storage

---
+++ packages/openauth/src/storage/sqlite.ts
@@ -0,0 +1,48 @@
+import {
+  joinKey,
+  splitKey,
+  type StorageAdapter,
+} from "@openauthjs/openauth/storage/storage";
+
+import Database from "libsql";
+
+export interface SqliteStorageOptions {
+  persist?: string;
+}
+export function SQLiteStorage(input?: SqliteStorageOptions): StorageAdapter {
+  // initialize sqlite database and create the necessary table structure
+  const db = new Database(input?.persist || ":memory:");
+  db.exec(
+    "CREATE TABLE IF NOT EXISTS storage (key TEXT PRIMARY KEY, value TEXT, ttl INTEGER)"
+  );
+
+  return {
+    async get(key: string[]) {
+      const joined = joinKey(key);
+      const row = db
+        .prepare("SELECT value FROM storage WHERE key = ?")
+        .get(joined) as { value: string } | undefined;
+      return row ? JSON.parse(row.value) : undefined;
+    },
+    async set(key: string[], value: any, ttl?: number) {
+      const joined = joinKey(key);
+      db.prepare(
+        "INSERT OR REPLACE INTO storage (key, value, ttl) VALUES (?, ?, ?)"
+      ).run(joined, JSON.stringify(value), ttl);
+    },
+    async remove(key: string[]) {
+      const joined = joinKey(key);
+      db.prepare("DELETE FROM storage WHERE key = ?").run(joined);
+    },
+    async *scan(prefix: string[]) {
+      const joined = joinKey(prefix);
+      const rows = db
+        .prepare("SELECT key, value FROM storage WHERE key LIKE ?")
+        .all(joined + "%") as { key: string; value: string }[];
+
+      for (const row of rows) {
+        yield [splitKey(row.key), JSON.parse(row.value)];
+      }
+    },
+  };
+}
