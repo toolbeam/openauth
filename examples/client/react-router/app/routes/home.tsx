600adf606b8e9b2fdb1a560608e352d05f57a2c2
Sami Al-Dury <git@aldury.com>
2024-12-10 14:13:21 +0100

    implement auth

--- examples/client/react-router/app/routes/home.tsx
+++ examples/client/react-router/app/routes/home.tsx
@@ -1,13 +1,28 @@
+import { tryAuth } from "~/auth";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { data, Form, Link } from "react-router";
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+export async function loader({ request }: Route.LoaderArgs) {
+  const auth = await tryAuth(request)
+  if (!auth) {
+    return { user: null }
+  }
+  return data({ user: auth.subject }, { headers: auth.headers })
 }
 
-export default function Home() {
-  return <Welcome />;
+export default function Page({ loaderData }: Route.ComponentProps) {
+  if (!loaderData.user) {
+    return (<div>
+      <p>You are not authorized.</p>
+      <Link to="/login">Login</Link>
+    </div>)
+  }
+
+  return (<div>
+    <p>Hello {loaderData.user.properties.email}</p>
+
+    <Form method="POST" action="/logout">
+      <button type="submit">Logout</button>
+    </Form>
+  </div>)
 }
