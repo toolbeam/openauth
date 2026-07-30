18d024732b39c25b15c19fe83aa6203509759a69
vitorfigmgs <vitorfigmarques@hotmail.com>
2026-07-30 18:11:09 -0300

    fix(custom-auth): Make react vite be an devDependency to avoid having the user installing it

--- packages/react/script/build.ts
+++ packages/react/script/build.ts
@@ -1,4 +1,9 @@
 import { Glob, $ } from "bun"
+import pkg from "../package.json" // Adju
+const externalDeps = [
+  ...Object.keys(pkg.peerDependencies || {}),
+  ...Object.keys(pkg.devDependencies || {}),
+]
 
 await $`rm -rf dist`
 const files = new Glob("./src/**/*.{ts,tsx}").scan()
@@ -6,7 +11,7 @@ for await (const file of files) {
   await Bun.build({
     format: "esm",
     outdir: "dist/esm",
-    external: ["*"],
+    external: [...externalDeps, "*"],
     root: "src",
     entrypoints: [file],
     define: {
